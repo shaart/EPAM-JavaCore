ea826e2494bd3626c42bc91386769f902ae2b1a3
Artur <shalaev-artur@yandex.ru>
2018-01-30 03:39:28 +0300

    Created AppWindow class for GUI
    Our HexMap is drawn on HexMapPanel

--- Projects/src/HexMap/Main.java
+++ Projects/src/HexMap/Main.java
@@ -1,10 +1,55 @@
 package HexMap;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JWindow;
+
+class AppWindow extends JWindow {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JFrame mainFrame;
+
+	public AppWindow() {
+		initGUI(400, 400, "HexMap");
+	}
+
+	public void show() {
+		mainFrame.setVisible(true);
+	}
+
+	private void initGUI(int width, int height, String title) {
+		mainFrame = new JFrame(title);
+		mainFrame.setSize(width, height);
+
+		mainFrame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent windowEvent) {
+				System.exit(0);
+			}
+		});
+
+		mainFrame.setVisible(true);
+		int defaultMapWidth = 15;
+		int defaultMapHeight = 7;
+		HexMapPanel hexMap = new HexMapPanel(defaultMapWidth, defaultMapHeight);
+		mainFrame.add(hexMap);
+	}
+}
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		AppWindow window = new AppWindow();
+		window.show();
 	}
-
 }
