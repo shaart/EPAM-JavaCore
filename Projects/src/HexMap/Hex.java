d8e7d2178a22a0487f80f842ae39ac57e141874d
Artur <shalaev-artur@yandex.ru>
2018-02-05 04:00:03 +0300

    Code refactoring at Hex

--- Projects/src/HexMap/Hex.java
+++ Projects/src/HexMap/Hex.java
@@ -1,52 +1,51 @@
 package HexMap;
 
 public class Hex {
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int hash = 1;
-		hash = prime + q;
-		hash = prime * hash + r;
-		return hash;
-	}
+    public final int q;
+    public final int r;
+    public final int s;
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Hex other = (Hex) obj;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int hash = prime + q;
+        hash = prime * hash + r;
 
-		return q == other.q && r == other.r;
-	}
+        return hash;
+    }
 
-	public final int q;
-	public final int r;
-	public final int s;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (obj instanceof Hex) {
+            Hex other = (Hex) obj;
+            return q == other.q && r == other.r;
+        }
 
-	/**
-	 * Creates default Hex with the default column (0) and the default row (0)
-	 */
-	public Hex() {
-		this(0, 0);
-	}
+        return false;
+    }
 
-	/**
-	 * Creates Hex with column (q) and row (r)
-	 * 
-	 * @param q
-	 *            Column
-	 * @param r
-	 *            Row
-	 */
-	public Hex(int q, int r) {
-		this.q = q;
-		this.r = r;
-		this.s = -q - r;
+    /**
+     * Creates default Hex with the default column (0) and the default row (0)
+     */
+    public Hex() {
+        this(0, 0);
+    }
 
-	}
+    /**
+     * Creates Hex with column (q) and row (r)
+     *
+     * @param q Column
+     * @param r Row
+     */
+    public Hex(int q, int r) {
+        this.q = q;
+        this.r = r;
+        this.s = -q - r;
+
+    }
 
 }
