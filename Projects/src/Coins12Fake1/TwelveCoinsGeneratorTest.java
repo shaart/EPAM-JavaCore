9664bbe5de631b1b9341cc741ac01bfedd5efc04
Artur <shalaev-artur@yandex.ru>
2018-02-11 21:12:25 +0300

    Added getRandom() border values tests

--- Projects/src/Coins12Fake1/TwelveCoinsGeneratorTest.java
+++ Projects/src/Coins12Fake1/TwelveCoinsGeneratorTest.java
@@ -6,6 +6,7 @@ import org.junit.rules.ExpectedException;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.*;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -15,6 +16,7 @@ public class TwelveCoinsGeneratorTest {
     public ExpectedException exception = ExpectedException.none();
 
     private final TwelveCoinsGenerator generator = new TwelveCoinsGenerator();
+    private final int ATTEMPTS_TO_GET_RANDOM = 1000;
 
     @Test
     public void getRandomIntInt() {
@@ -48,6 +50,35 @@ public class TwelveCoinsGeneratorTest {
                     fail("Catch unknown exception when invoked getRandom(int,int) with min = 100, max = 0");
                 }
             }
+        } catch (java.lang.NoSuchMethodException e) {
+            fail("Method getRandom(int, int) not found");
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void getRandomIntIntBorderValues() {
+        try {
+            Method getRandom = generator.getClass().getDeclaredMethod("getRandom", int.class, int.class);
+            getRandom.setAccessible(true);
+
+            int number;
+            int min = 0;
+            int max = 3;
+            boolean wasMin = false;
+            boolean wasMax = false;
+            for (int i = 0; i < ATTEMPTS_TO_GET_RANDOM && (!wasMin || !wasMax); i++) {
+                number = (int) getRandom.invoke(generator, min, max);
+                if (number == min) {
+                    wasMin = true;
+                }
+                if (number == max) {
+                    wasMax = true;
+                }
+            }
+            assertTrue(wasMax && wasMax);
+
         } catch (java.lang.NoSuchMethodException e) {
             fail("Method getRandom(int, int) not found");
         } catch (Exception e) {
@@ -99,3 +130,32 @@ public class TwelveCoinsGeneratorTest {
         }
     }
 }
+
+    @Test
+    public void getRandomDoubleDoubleBorderValues() {
+        try {
+            Method getRandom = generator.getClass().getDeclaredMethod("getRandom", double.class, double.class);
+            getRandom.setAccessible(true);
+
+            double number;
+            double min = 0.55;
+            double max = 3.33;
+            boolean wasMin = false;
+            boolean wasMax = false;
+            for (int i = 0; i < ATTEMPTS_TO_GET_RANDOM && (!wasMin || !wasMax); i++) {
+                number = (double) getRandom.invoke(generator, min, max);
+                if (Math.round(number * 100.0) / 100.0 == min) {
+                    wasMin = true;
+                }
+                if (Math.round(number * 100.0) / 100.0 == max) {
+                    wasMax = true;
+                }
+            }
+            assertTrue(wasMax && wasMax);
+
+        } catch (java.lang.NoSuchMethodException e) {
+            fail("Method getRandom(int, int) not found");
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
