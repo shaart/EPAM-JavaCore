85cc12b639be34f5260cb36be43c56898b51348d
Artur <shalaev-artur@yandex.ru>
2018-02-13 02:21:32 +0300

    Main now generates coins 12 times and prints result

--- Projects/src/Coins12Fake1/Main.java
+++ Projects/src/Coins12Fake1/Main.java
@@ -5,22 +5,16 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<Coin> twelveCoins = TwelveCoinsGenerator.generate();
+        List<Coin> twelveCoins;
+        Coin fake;
 
-        Coin fake = FakeSearcher.find(twelveCoins);
-        System.out.format("Fake coin have weight %f\n", fake.getWeight());
-
-//        for (Coin coin : twelveCoins) {
-//            if (coin == fake) continue;
-//
-//            System.out.format("Weight of common coin: %f\n", coin.getWeight());
-//            break;
-//
-//        }
-
-        System.out.println("All coins weights: ");
-        for (Coin coin : twelveCoins) {
-            System.out.format("Coin weight %f\n", coin.getWeight());
+        for (int i = 0; i < 12; i++) {
+            System.out.println();
+            twelveCoins = TwelveCoinsGenerator.generate(i);
+            fake = FakeSearcher.find(twelveCoins);
+            System.out.format("{ %s }\n", fake);
+            System.out.format("Fake coin have weight %f\n", fake.getWeight());
+            System.out.format("Position of Fake coin: %d\n", twelveCoins.indexOf(fake));
         }
     }
 
