78022b3bf529a2ce5375d2b46de1e8fedea0856e
Artur <imshaart@gmail.com>
2018-03-09 00:23:38 +0300

    Implemented non-recursive StackOverflowError

--- Projects/src/JavaErrors/JavaStackOverflowError/WithoutRecursiveMethods.java
+++ Projects/src/JavaErrors/JavaStackOverflowError/WithoutRecursiveMethods.java
@@ -6,6 +6,82 @@ package JavaErrors.JavaStackOverflowError;
  */
 public class WithoutRecursiveMethods {
     public static void main(String[] args) {
-        throw new UnsupportedOperationException();
+        try {
+            // But this is implicit recursion
+            overriddenToString();
+        } catch (Exception | Error e) {
+            showError(e);
+        } finally {
+            System.out.println();
+        }
+
+        try {
+            // Two classes with "has-a" relation to each other
+            twoClassesEachContainsAnother();
+        } catch (Exception | Error e) {
+            showError(e);
+        } finally {
+            System.out.println();
+        }
+
+        try {
+            // Or we can just do this
+            justThrow();
+        } catch (Exception | Error e) {
+            showError(e);
+        } finally {
+            System.out.println();
+        }
+    }
+
+    private static void showError(Throwable e) {
+        System.out.println("Catch: " + e.getClass());
+        if (e instanceof StackOverflowError) {
+            StackOverflowError soe = (StackOverflowError) e;
+            StackTraceElement[] stackTraceElements = soe.getStackTrace();
+            String cause = (stackTraceElements.length > 0 ? stackTraceElements[0].toString() : "none");
+            System.out.println("Cause: " + cause);
+            System.out.println("Stack trace length: " + stackTraceElements.length);
+        }
+    }
+
+    // ----------------------------------------------------------------
+    private static class OverriddenToString {
+        private String value;
+
+        @Override
+        public String toString() {
+            return value + this;
+        }
+    }
+
+    private static void overriddenToString() {
+        System.out.println(new OverriddenToString());
+    }
+
+    // ----------------------------------------------------------------
+    private static class Son {
+        private Daddy daddy;
+
+        Son() {
+            daddy = new Daddy();
+        }
+    }
+
+    private static class Daddy {
+        private Son son;
+
+        Daddy() {
+            son = new Son();
+        }
+    }
+
+    private static void twoClassesEachContainsAnother() {
+        Daddy daddy = new Daddy();
+    }
+
+    // ----------------------------------------------------------------
+    private static void justThrow() {
+        throw new StackOverflowError("It's legal too");
     }
 }
