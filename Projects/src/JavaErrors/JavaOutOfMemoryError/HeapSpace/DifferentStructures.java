89103641dc2ba574140cca891c1cf63e9d8a5adf
Artur <imshaart@gmail.com>
2018-03-10 13:52:29 +0300

    2 ways to get "out of memory: java heap space" implemented

--- Projects/src/JavaErrors/JavaOutOfMemoryError/HeapSpace/DifferentStructures.java
+++ Projects/src/JavaErrors/JavaOutOfMemoryError/HeapSpace/DifferentStructures.java
@@ -1,5 +1,8 @@
 package JavaErrors.JavaOutOfMemoryError.HeapSpace;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * [Task 1] java.lang.OutOfMemoryError: Java heap space.
  * You can use different data structures.
@@ -7,6 +10,42 @@ package JavaErrors.JavaOutOfMemoryError.HeapSpace;
  */
 public class DifferentStructures {
     public static void main(String[] args) {
-        throw new UnsupportedOperationException();
+        try {
+            String curArg = args.length > 0 ? args[0] : "";
+            switch (curArg) {
+                case "0":
+                    makeBigArrays();
+                    break;
+                case "1":
+                default:
+                    saveSmallArrays();
+                    break;
+            }
+        } catch (Exception | Error e) {
+            System.out.println("Catch: " + e.getClass());
+            if (e instanceof OutOfMemoryError) {
+                System.out.println("Cause: " + e.getMessage());
+            }
+        } catch (Throwable t) {
+            System.out.println("Catch: " + t.getClass());
+        }
+    }
+
+    //-------------------------------------------------
+    private static void makeBigArrays() {
+        long[] array;
+        while (true) {
+            array = new long[1_000_000_000];
+        }
+    }
+
+    //-------------------------------------------------
+    private static void saveSmallArrays() {
+        List<long[]> list = new ArrayList<>();
+        long[] array;
+        while (true) {
+            array = new long[10_000];
+            list.add(array);
+        }
     }
 }
