8d7810846b5f2429047f2accd3065e9b86c978e9
Artur <imshaart@gmail.com>
2018-03-10 16:43:17 +0300

    JVisualVM package initial commit

---
+++ Projects/src/JavaErrors/JVisualVM/Main.java
@@ -0,0 +1,13 @@
+package JavaErrors.JVisualVM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Object> list = new ArrayList<>();
+        while (true) {
+            list.add(new Object());
+        }
+    }
+}
