65d57417bc6b1d8d7e28ce682e44c1c18db6bcce
Artur <imshaart@gmail.com>
2018-03-10 22:43:32 +0300

    JVisualVM task implemented

--- Projects/src/JavaErrors/JVisualVM/Main.java
+++ Projects/src/JavaErrors/JVisualVM/Main.java
@@ -5,9 +5,39 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // Uncomment one of these methods
+//        initialCode();
+//        callGCEveryTime();
+        moreFrequentPeaks();
+    }
+
+    private static void initialCode() {
         List<Object> list = new ArrayList<>();
         while (true) {
             list.add(new Object());
+        }
+    }
+
+    private static void callGCEveryTime() {
+        List<Object> list = new ArrayList<>();
+        while (true) {
+            list.add(new Object());
+            System.gc();
+        }
+    }
+
+    private static void moreFrequentPeaks() {
+        int i = 0;
+        while (true) {
+            new Object();
+            i++;
+
+            if (i % 1_000_000_000 == 0) {
+                System.gc();
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) { /* Nothing to do*/ }
+            }
         }
     }
 }
